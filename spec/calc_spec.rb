d02bd882e75b6b56902b21b186d07eaf3331d927
GitLabHQ Admin User <admin@local.host>
2012-10-02 06:14:43 +0300

    initial tests for calc project

---
+++ spec/calc_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe Calc do
+  describe "get method" do
+    it "should receive array as argument" do
+
+    end
+
+    it "should receive number as argument" do
+
+    end
+  end
+
+  describe "plus method" do
+    it "should summarize numbers for the array argument" do
+
+    end
+
+    it "should summarize numbers for the number argument" do
+
+    end
+  end
+
+  describe "minus method" do
+    it "should subtract numbers for the array argument" do
+
+
+    end
+
+    it "shoule subtract numbers fro the number argument" do
+
+
+    end
+  end
+
+  describe "cleaning the stack" do
+    it "plus method" do
+
+    end
+
+    it "minus mehtod" do
+
+    end
+  end
+end
